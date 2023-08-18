9ba4e612e86fc641fed8c3373007db67aa307f30
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-18 20:45:03 +0200

    add auth protection to all pages (exclude "/")

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +15,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  <ClerkProvider>
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
+  </ClerkProvider>
 }
