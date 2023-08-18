a50e2e93f1113a80a1e243b7f3e0cece7f5be765
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-18 20:33:44 +0200

    update page title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'GenAI',
-  description: 'AIaaS Platform',
+  title: 'GenAIaaS',
+  description: 'Generative AIaaS Platform',
 }
 
 export default function RootLayout({
