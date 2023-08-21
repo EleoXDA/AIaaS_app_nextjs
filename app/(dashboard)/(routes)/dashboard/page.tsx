32d1ee82127c9dc5f83d6df94c3d397b2f577a59
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-21 23:30:36 +0200

    add UserButton to Dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <h1>Dashboard Page (Protected)</h1>
-      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
