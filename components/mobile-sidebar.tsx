5603378f2dd84a6c722967485f978d32a6cb101c
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-21 23:55:56 +0200

    + sheet & sheettrigger @Shadcn for mobile sidebar

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { Menu } from "lucide-react";
+
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./sidebar";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="md:hidden" />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default MobileSidebar;
