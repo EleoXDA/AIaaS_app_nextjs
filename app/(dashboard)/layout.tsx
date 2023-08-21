9d9d5eec23967fbc2ef272661b40e48489b4868e
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-21 23:32:07 +0200

    import both Navbar & Sidebar into Dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 const DashboardLayout = ({ 
   children 
 } :{
@@ -7,11 +10,10 @@ const DashboardLayout = ({
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0
       z-[80] bg-gray-900">
-        <div>
-          Hello Sidebar
-        </div>
+        <Sidebar />
       </div>
       <main className="md:pl-72">
+        <Navbar />
         {children}
       </main>
     </div>
