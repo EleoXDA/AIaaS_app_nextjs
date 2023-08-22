23f4399631473abf88e90c651d631fd04814824e
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-22 09:27:37 +0200

    detect current screen

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,8 +3,17 @@
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
     <Sheet>
       <SheetTrigger>
