2c6185a0cd6e60c8bc5a03fdbda9f29466de5007
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-22 09:27:55 +0200

    adjust display according to the current pathway

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -53,12 +54,13 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
 <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative h-8 w-8 mr-4">
-            <Image fill alt="Logo" src="/logo.png" />
+            <Image fill alt="Logo" src="/logo.png" sizes="width: auto, height:auto"/>
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
             GenAIaaS
@@ -68,7 +70,7 @@ const Sidebar = () => {
           {routes.map((route) => (
             <Link
               key={route.href} 
-              href={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+              href={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
               <div className="flex items-center flex-1">
                 <route.icon className={cn("w-5 h-5 mr-3", route.color)} />
                 {route.label}
