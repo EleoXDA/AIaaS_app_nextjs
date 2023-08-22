599c13df2238295ea980d041a65adb4ac447b65e
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-22 22:56:54 +0200

    update dashboard description

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -52,10 +52,10 @@ const DashboardPage = () => {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the Power of AI
+          Generative AI as a Service
         </h2>
         <p className="text-muted-foregroud font-light text-sm md:text-lg text-center">
-          GenAIaaS is a platform that allows you to get the power of AI.
+          GenAIaaS allows you to use the power of GPT-3.5.
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
