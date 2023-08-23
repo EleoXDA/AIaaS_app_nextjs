e814aa4e974b567d96b00d2b21e1395c785284cc
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-23 23:37:46 +0200

    create images page and route

---
+++ app/api/image/route.ts
@@ -0,0 +1,50 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import OpenAI from "openai" 
+
+const openai = new OpenAI ({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+export async function POST(
+  req: Request
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = 1, resolution = "256x256"  } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!prompt) {
+      return new NextResponse("Prompt is required", { status: 400 });
+    }
+    if (!amount) {
+      return new NextResponse("Amount is required", { status: 400 });
+    }
+    if (!resolution) {
+      return new NextResponse("Resolution is required", { status: 400 });
+    }
+
+    const response = await openai.images.generate({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
+    
+    return new NextResponse(JSON.stringify(response.data))
+
+
+
+
+    // if (!isPro) {
+    //   await incrementApiLimit();
+    // }
+
+    // return NextResponse.json(response.data.choices[0].message);
+  } catch (error) {
+    console.log('[IMAGE_ERROR]', error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
