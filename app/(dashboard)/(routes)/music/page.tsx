96404c5588c059ede22885b2c380e04b5fe1f6a5
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-27 15:15:18 +0200

    reset image on empty screen and adjust padding

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -60,55 +60,59 @@ const MusicPage = () => {
         bgColor="bg-emerald-500/10"
       />
       <div className="px-4 lg:px-8">
-        <Form {...form}>
-          <form 
-            onSubmit={form.handleSubmit(onSubmit)} 
-            className="
-              rounded-lg 
-              border 
-              w-full 
-              p-4 
-              px-3 
-              md:px-6 
-              focus-within:shadow-sm
-              grid
-              grid-cols-12
-              gap-2
-            "
-          >
-            <FormField
-              name="prompt"
-              render={({ field }) => (
-                <FormItem className="col-span-12 lg:col-span-10">
-                  <FormControl className="m-0 p-0">
-                    <Input
-                      className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                      disabled={isLoading} 
-                      placeholder="Please describe the music you are looking for..." 
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
-              Generate
-            </Button>
-          </form>
-        </Form>
-        {isLoading && (
-          <div className="p-20">
-            <Loader />
-          </div>
-        )}
-        {!music && !isLoading && (
-          <Empty label="" />
-        )}
-        {music && (
-          <audio controls className="w-full mt-8">
-            <source src={music} />
-          </audio>
-        )}
+        <div>
+          <Form {...form}>
+            <form 
+              onSubmit={form.handleSubmit(onSubmit)} 
+              className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading} 
+                        placeholder="Please describe the music you are looking for..." 
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-20">
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && (
+            <Empty label="" />
+          )}
+          {music && (
+            <audio controls className="w-full mt-8">
+              <source src={music} />
+            </audio>
+          )}
+        </div>
       </div>
     </div>
    );
--- components/ui/empty.tsx
+++ components/ui/empty.tsx
@@ -8,9 +8,9 @@ export const Empty = ({
   label
 }: EmptyProps) => {
   return (
-    <div className="h-full p-20 flex flex-col items-center justify-center">
+    <div className="h-full p-5 flex flex-col items-center justify-center">
       <div className="relative h-110 w-409">
-        <Image src="/GenAIaaS.png" width={409} height={110} alt="Empty" />
+        <Image src="/GenAIaaS_big.png" width={409} height={110} alt="Empty" />
       </div>
       <p className="text-muted-foreground text-sm text-center">
         {label}
