39b0818314768af16f6cea59fab967c1e04edacb
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2023-08-27 21:20:56 +0200

    Fix: sidebar overlapped pro modal when width

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -14,7 +14,7 @@ const DashboardLayout = async ({
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0
-      z-[80] bg-gray-900">
+      z-[1] bg-gray-900">
         <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72">
